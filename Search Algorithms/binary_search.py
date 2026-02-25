d2bdfbd0284dc930a8a7ff5bd020ab13f32cc624
Dr Kambhampati Venkata Sambasiva Rao <kvsambasivarao65@gmail.com>
2026-02-25 15:02:51 +0530

    Create binary_search.py

---
+++ Search Algorithms/binary_search.py
@@ -0,0 +1,30 @@
+# File Name: binary_search.py
+
+def binary_search(arr, key):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+
+# Driver Code
+n = int(input("Enter number of elements: "))
+arr = list(map(int, input("Enter sorted elements separated by space: ").split()))
+key = int(input("Enter key to search: "))
+
+result = binary_search(arr, key)
+
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found")
