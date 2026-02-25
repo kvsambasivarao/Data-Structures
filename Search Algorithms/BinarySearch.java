2ab16f20a7d9d590ce63ae4b544fac67872185fb
Dr Kambhampati Venkata Sambasiva Rao <kvsambasivarao65@gmail.com>
2026-02-25 15:02:32 +0530

    Create BinarySearch.java

---
+++ Search Algorithms/BinarySearch.java
@@ -0,0 +1,47 @@
+// File Name: BinarySearch.java
+
+import java.util.Scanner;
+
+class BinarySearch {
+
+    static int binarySearch(int[] arr, int key) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " sorted elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.print("Enter key to search: ");
+        int key = sc.nextInt();
+
+        int result = binarySearch(arr, key);
+
+        if (result != -1)
+            System.out.println("Element found at index " + result);
+        else
+            System.out.println("Element not found");
+
+        sc.close();
+    }
+}
