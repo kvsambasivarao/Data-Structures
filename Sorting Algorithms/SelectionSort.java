f63abf0efe3c3b29fa810b25022e44e3726d608c
Dr Kambhampati Venkata Sambasiva Rao <kvsambasivarao65@gmail.com>
2026-02-26 12:11:54 +0530

    Create SelectionSort.java

---
+++ Sorting Algorithms/SelectionSort.java
@@ -0,0 +1,37 @@
+// File Name: SelectionSort.java
+
+public class SelectionSort {
+
+    static void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void printArray(int arr[]) {
+        for (int value : arr)
+            System.out.print(value + " ");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+}
