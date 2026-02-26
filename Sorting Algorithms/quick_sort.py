4cb8a23e1b5a5408be7ff26ae744cd9f24c49a70
Dr Kambhampati Venkata Sambasiva Rao <kvsambasivarao65@gmail.com>
2026-02-26 12:16:54 +0530

    Create quick_sort.py

---
+++ Sorting Algorithms/quick_sort.py
@@ -0,0 +1,27 @@
+# File Name: quick_sort.py
+
+def partition(arr, low, high):
+    pivot = arr[high]   # Last element as pivot
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)   # Left
+        quick_sort(arr, pi + 1, high)  # Right
+
+# Main Program
+arr = [10, 7, 8, 9, 1, 5]
+
+quick_sort(arr, 0, len(arr) - 1)
+
+print("Sorted array:", arr)
