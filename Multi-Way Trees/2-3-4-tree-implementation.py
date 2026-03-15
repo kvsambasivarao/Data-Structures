4cb2ea9659ed9d479cb34ce728eb9a34df43764d
Dr Kambhampati Venkata Sambasiva Rao <kvsambasivarao65@gmail.com>
2026-03-15 20:55:52 +0530

    Create 2-3-4-tree-implementation.py

---
+++ Multi-Way Trees/2-3-4-tree-implementation.py
@@ -0,0 +1,62 @@
+# File: 2-3-4-tree-implementation.py
+# Program implementing 2-3-4 Tree operations
+
+class Node:
+
+    def __init__(self):
+        self.keys = []
+        self.children = []
+        self.leaf = True
+
+
+class TwoThreeFourTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def traverse(self,node):
+
+        for i in range(len(node.keys)):
+
+            if not node.leaf:
+                self.traverse(node.children[i])
+
+            print(node.keys[i], end=" ")
+
+        if not node.leaf:
+            self.traverse(node.children[len(node.keys)])
+
+    def search(self,node,key):
+
+        i=0
+
+        while i < len(node.keys) and key > node.keys[i]:
+            i+=1
+
+        if i < len(node.keys) and key == node.keys[i]:
+            return node
+
+        if node.leaf:
+            return None
+
+        return self.search(node.children[i],key)
+
+    def insert(self,key):
+
+        if len(self.root.keys) < 3:
+            self.root.keys.append(key)
+            self.root.keys.sort()
+
+
+if __name__ == "__main__":
+
+    tree = TwoThreeFourTree()
+
+    tree.insert(10)
+    tree.insert(20)
+    tree.insert(5)
+    tree.insert(6)
+
+    print("Traversal of 2-3-4 Tree:")
+
+    tree.traverse(tree.root)
